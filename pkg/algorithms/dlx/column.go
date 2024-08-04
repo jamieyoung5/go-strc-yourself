96a2637f9f4f741040120ebcf7f627b4076bb2e2
jamieyoung5 <jamie5young55@gmail.com>
2024-08-05 00:44:54 +0100

    add dlx agorithm

---
+++ pkg/algorithms/dlx/column.go
@@ -0,0 +1,35 @@
+package dlx
+
+type Column struct {
+	*Node
+	ID   string
+	Size int
+}
+
+// Cover unlinks the column and rows that have a node in that column from the rest of the matrix
+func (c *Column) Cover() {
+	c.Left.Right = c.Right
+	c.Right.Left = c.Left
+
+	for node := c.Down; node != c.Node; node = node.Down {
+		for rowNode := node.Right; rowNode != node; rowNode = rowNode.Right {
+			rowNode.Up.Down = rowNode.Down
+			rowNode.Down.Up = rowNode.Up
+			rowNode.Column.Size--
+		}
+	}
+}
+
+// Uncover re-links the column and respective rows to the rest of the matrix
+func (c *Column) Uncover() {
+	for node := c.Up; node != c.Node; node = node.Up {
+		for rowNode := node.Left; rowNode != node; rowNode = rowNode.Left {
+			rowNode.Up.Down = rowNode
+			rowNode.Down.Up = rowNode
+			rowNode.Column.Size++
+		}
+	}
+
+	c.Left.Right = c.Node
+	c.Right.Left = c.Node
+}
---
+++ pkg/algorithms/dlx/dlx.go
@@ -0,0 +1,155 @@
+package dlx
+
+import (
+	"fmt"
+	"math"
+)
+
+const maxSolutions = math.MaxInt8
+
+type Matrix struct {
+	Root *Node
+}
+
+type Node struct {
+	Left, Right, Up, Down *Node
+	Column                *Column
+}
+
+// NewMatrix generates columns and each columns' respective header node
+func NewMatrix(columnIDs []string) *Matrix {
+	root := &Column{Node: &Node{}}
+	root.Left = root.Node
+	root.Right = root.Node
+
+	lastHeader := root
+	for _, id := range columnIDs {
+		newHeader := &Column{ID: id}
+		newHeader.Node = &Node{Left: lastHeader.Node, Right: root.Node, Column: newHeader}
+		newHeader.Up = newHeader.Node
+		newHeader.Down = newHeader.Node
+
+		lastHeader.Right = newHeader.Node
+		root.Left = newHeader.Node
+		lastHeader = newHeader
+	}
+
+	return &Matrix{Root: root.Node}
+}
+
+func (m *Matrix) PrintMatrix() {
+	currentHeader := m.Root.Right // Start from the first actual header, not the root itself
+	for currentHeader != m.Root {
+		fmt.Printf("Column ID: %s, Size: %d\n", currentHeader.Column.ID, currentHeader.Column.Size)
+		currentNode := currentHeader.Down
+		for currentNode != currentHeader { // Iterate until we loop back to the dummy node
+			fmt.Println("  Node at", currentNode)
+			currentNode = currentNode.Down
+		}
+		currentHeader = currentHeader.Right
+	}
+}
+
+// AppendRow adds a new node to the bottom of every column included in columnIds and links them together to form a row
+func (m *Matrix) AppendRow(columnIds []string) {
+	idSet := make(map[string]struct{})
+	for _, id := range columnIds {
+		idSet[id] = struct{}{}
+	}
+
+	var firstNode, lastNode *Node
+
+	header := m.Root.Right
+	for header != m.Root {
+		if _, exists := idSet[header.Column.ID]; exists {
+			newNode := &Node{
+				Column: header.Column,
+				Up:     header.Up,
+				Down:   header,
+			}
+
+			// Link vertically
+			newNode.Up.Down = newNode
+			newNode.Down.Up = newNode
+			header.Column.Size++
+
+			// Link horizontally
+			if firstNode == nil {
+				firstNode = newNode
+				lastNode = newNode
+			} else {
+				newNode.Left = lastNode
+				lastNode.Right = newNode
+				lastNode = newNode
+			}
+		}
+		header = header.Right
+	}
+
+	// Close the horizontal loop
+	if firstNode != nil {
+		if lastNode == firstNode {
+			// If there is only one node, it should point to itself
+			firstNode.Left = firstNode
+			firstNode.Right = firstNode
+		} else {
+			firstNode.Left = lastNode
+			lastNode.Right = firstNode
+		}
+	}
+}
+
+func (m *Matrix) Search(solution []*Node, solutions *[][]*Node) {
+	if len(*solutions) >= maxSolutions {
+		return
+	}
+
+	// Check if there are no more columns to cover
+	if m.Root.Right == m.Root {
+		// All columns are covered, hence a solution is found
+		// Copy the solution to avoid referencing the same slice
+		solved := make([]*Node, len(solution))
+		copy(solved, solution)
+		*solutions = append(*solutions, solved)
+		return
+	}
+
+	// Choose the column with the fewest nodes to minimize the branching factor
+	c := m.getColumnWithFewestNodes()
+
+	c.Cover()
+
+	for r := c.Down; r != c.Node; r = r.Down {
+		solution = append(solution, r)
+
+		// Cover all columns in this row
+		for j := r.Right; j != r; j = j.Right {
+			j.Column.Cover()
+		}
+
+		m.Search(solution, solutions)
+
+		// Uncover all columns in this row for backtracking
+		for j := r.Left; j != r; j = j.Left {
+			j.Column.Uncover()
+		}
+
+		// Remove the row from the solution set
+		solution = solution[:len(solution)-1]
+	}
+
+	c.Uncover()
+}
+
+// getColumnWithFewestNodes get the smallest column in the matrix
+func (m *Matrix) getColumnWithFewestNodes() *Column {
+	minSize := int(^uint(0) >> 1) // Max int value
+	chosenColumn := m.Root.Right.Column
+	for c := m.Root.Right; c != m.Root; c = c.Right {
+		if c.Column.Size < minSize {
+			minSize = c.Column.Size
+			chosenColumn = c.Column
+		}
+	}
+	return chosenColumn
+}
