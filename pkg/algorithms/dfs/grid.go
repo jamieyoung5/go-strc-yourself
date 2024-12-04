881013629821dd75acc76f54875e9ee1a285e36a
jamieyoung5 <jamie5young55@gmail.com>
2024-12-04 09:27:13 +0000

    feat: add slice utils

---
+++ pkg/algorithms/dfs/grid.go
@@ -0,0 +1,6 @@
+package dfs
+
+// SearchGrid searches for occurences of a 'pattern' in a grid.
+func SearchGrid[T comparable](grid [][]T, pattern []T) int {
+
+}
---
+++ pkg/sliceutil/slices.go
@@ -0,0 +1,36 @@
+package sliceutil
+
+// Reverse reverses a slice
+func Reverse[T any](s []T) []T {
+	reversed := make([]T, len(s))
+	copy(reversed, s)
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		reversed[i], reversed[j] = reversed[j], reversed[i]
+	}
+	return reversed
+}
+
+// Equal compares two slices contents and returns true if they are the same, false if they are not
+func Equal[T comparable](a, b []T) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func Counts[V comparable](vs []V) map[V]int {
+	return CountsFunc(vs, func(v V) V { return v })
+}
+
+func CountsFunc[V any, K comparable](vs []V, key func(V) K) map[K]int {
+	h := make(map[K]int)
+	for _, v := range vs {
+		h[key(v)]++
+	}
+	return h
+}
